3bb279e0d44d2e8672556110a884b6a0f603408d
p4ul0x10 <64813055+p4ul0x10@users.noreply.github.com>
2021-02-11 22:27:33 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -13,6 +13,9 @@
 #include "assets/libs/set_routes.h"
 #include "assets/libs/engine_balls.h"
 #include "assets/libs/check_mods.h"
+#include "assets/libs/machine_trace.h"
+#include "assets/libs/machine_make_route.h"
+#include "assets/libs/libunits.h"
 
 int main(int argc, char *argv[]){
 
@@ -40,14 +43,14 @@ int main(int argc, char *argv[]){
     //move branca lisa vars
     float f_cue, calc_cue, divAL, divLA, add_v;
     float f = 0, al;
-    int init_mode[1], vetN[16], vetM[16], all_k[1] ,e_x[1], e_y[1];
+    int cont_play = 0, init_mode[1], vetN[16], vetM[16], all_k[1] ,e_x[1], e_y[1];
+    int cont_units[1], cont_ball[15], cont_p[1], cont_m[1], cont_all[1];
     float eq_y, div_eq_y, percent_y_1, percent_y_2, percent_y;
     float vetAX[16], vetAY[16], vetV[16], vetF[16], init_x[16], vang[16], init_y[16], return_x[1], return_y[1];
     float vetX[16];
     float vetY[16];
     float p_x[1], p_y[1];
-
-    char random_units[1], mode_play[1];
+    char random_units[1], mode_play[1], rand_on[1], status_check[1], mode_player[2], change_mode[1];
     random_units[0] = 'f';
     p_x[0] = 0;
     p_y[0] = 0;
@@ -59,13 +62,30 @@ int main(int argc, char *argv[]){
     const char* SDL_GetKeyName(SDL_Keycode key);
 
     init_random(random_units, vetN, vetM, vetV, vetF, vetX, vetY, init_x, init_y);
+    
+    //
+    int b = 0;
+    while(b < 15){
 
+        cont_ball[b] = 0;
+        b++;
+    }
+    //
+    
     //start init coord x y effect img
     e_y[0] = (40 / 2) + 465;
     e_x[0] = (40 / 2) + 750;
     //end init coord x y effect img
-
-    while (1==1){
+    
+    srand (time(NULL));
+    activePlay = rand() % 2+1;
+    cont_units[0] = 0;
+    rand_on[0] = 'n';
+    status_check[0] = 'n';
+    change_mode[0] = 't';
+    mode_player[0] = '0';
+    mode_player[1] = '0';
+    while (true){
 
         SDL_RenderClear(renderer);
 		loadMesa(renderer);
@@ -93,6 +113,7 @@ int main(int argc, char *argv[]){
                         vetY[r] = 0;
                         vetAX[r] = 0;
                         vetAY[r] = 0;
+                        cont_ball[r] = 0;
                     }
 
                     all_k[0] = 0;
@@ -103,6 +124,17 @@ int main(int argc, char *argv[]){
                     vetY[15] = 217;
                     vetX[15] = 317;
                     vetM[15] = 0;
+                    f = 0;
+                    cont_play = 0;
+                    cont_units[0] = 0;
+                    rand_on[0] = 'n';
+                    status_check[0] = 'n';
+                    mode_player[0] = '0';
+                    mode_player[1] = '0';
+                    change_mode[0] = 't'; 
+                    srand (time(NULL));
+                    activePlay = rand() % 2+1;
+
                 }else if(event.key.keysym.sym == SDLK_SPACE && mode_play[0] != 'f'){
 
                     if(f > 0){
@@ -133,7 +165,7 @@ int main(int argc, char *argv[]){
 				}else if (event.key.keysym.sym == SDLK_j && mode_play[0] != 'f'){
                     //start player if force != 0
 
-					if (f > 0 && activePlay == 0){
+					if (f > 0 && activePlay == 2){
 						// SDL_StopTextInput();
                         all_k[0] = 0;
                         init_x[15] = vetX[15];
@@ -141,9 +173,13 @@ int main(int argc, char *argv[]){
                         vetM[15] = init_mode[0];
                         vetV[15] = add_v;
                         vetF[15] = add_v;
-    					activePlay = 1;
+    					activePlay = 2;
+                        change_mode[0] = 'n';
+                        cont_play++;
 
 				    }
+
+                    check_units(cont_m, cont_p, vetN, vetX, vetY, mode_player);
                     //end player if force != 0
 
                 }else if(event.key.keysym.sym == SDLK_b && mode_play[0] == 'f'){
@@ -226,6 +262,10 @@ int main(int argc, char *argv[]){
                     printf("Key %s = quit", SDL_GetKeyName(event.key.keysym.sym));
 
                     printf("\nSNOOKER GAME END\n");
+                    
+                    SDL_DestroyWindow(win);
+                    SDL_Quit();
+                    
                     return 0;
 
                 }
@@ -263,7 +303,7 @@ int main(int argc, char *argv[]){
                             return_y[0] = miray_central;
                         }
 
-                        if(activePlay == 0 && vetM[15] > 3 && mode_play[0] != 'f'){ // player && machine mode of
+                        if(activePlay == 2 && vetM[15] > 3 && mode_play[0] != 'f'){ // player && machine mode of
 
                             mouseX = event.motion.x;
                             mouseY = event.motion.y;
@@ -342,33 +382,229 @@ int main(int argc, char *argv[]){
     			//end window close event
 		}
 
-
         if(vetX[15] == 0 && vetY[15] == 0){
 
             mode_play[0] = 'f';
         }
 
+        if(activePlay == 1 && change_mode[0] == 't'){
+     
+            machine_route(vetN, vetM, vetV, vetF, vetX, vetY, vetAX, vetAY, rand_on, mode_player);
+            check_units(cont_m, cont_p, vetN, vetX, vetY, mode_player);
+            cont_play++;
+            change_mode[0] = 'n';
+            
+            if(cont_play >= 3){
+                //SDL_DestroyWindow(win);
+                //SDL_Quit();
+            }
+
+            sleep(2);
+        }
+
         engine_show_all(renderer, vetN, vetM, vetV, vetF, vetX, vetY, vetAX, vetAY, all_k, init_x, init_y, e_x, e_y);
 
         //start reset mode off
-        if(activePlay == 1 && vetM[15] == 100){
+        if(change_mode[0] == 'n' && vetM[15] == 100 && cont_play >= 1 && activePlay == 1 || activePlay == 2){
 
-            if(all_k[0] >= 15){
+            if(change_mode[0] == 'n'){
 
-                activePlay = 0;
-                f = 0;
-                add_v = 0;
+                //SDL_Quit();
+                //SDL_DestroyWindow(win);
 
-                //reset effect adds
-                e_x[0] = (40 / 2) + 750;
-                e_y[0] = (40 / 2) + 465;
-                //reset effect adds  
+                if(all_k[0] >= 15){
+
+                    //activePlay = 0;
+                    f = 0;
+                    add_v = 0;
+
+                    //reset effect adds
+                    e_x[0] = (40 / 2) + 750;
+                    e_y[0] = (40 / 2) + 465;
+                    //reset effect adds  
+                }
+
+                if(vetM[15] == 100 && vetV[15] == 0 && status_check[0] != 't'){
+
+                    bool num_checked = false;
+                    int cont_b, cont_s = 1;
+                    cont_b = cont_units[0];
+
+                    for(int s = 0; s < 15; s++){ 
+                           
+                        if(vetV[s] == 0 && vetM[s] > 3 && vetAX[s] == 0 && vetAY[s] == 0){
+                            cont_s++;
+                        }
+                        /*for(int c = 14; c >= 0; c--){
+                         
+                            if(vetX[c] == 0 && vetY[c] == 0){
+
+                                if(cont_ball[s] == vetN[c]){
+
+                                    num_checked = true;
+                                }   
+                            }
+
+                            if(c == 0 && num_checked == false){
+
+                                cont_ball[s] = vetN[c];
+                                cont_units[0] = cont_units[0] + 1;
+                            }
+                        }*/ 
+                    
+                    }
+
+                    if(cont_s > 15){
+
+                        for(int ss = 0; ss < 15; ss++){
+
+                            if(vetN[ss] != 0 && vetX[ss] == 0 && vetY[ss] == 0){
+                            
+                                if(vetN[ss] != cont_ball[0]){
+
+                                    cont_ball[cont_b] = vetN[ss];
+                                    cont_units[0] = cont_b + 1;
+                                }
+                            }
+                        }
+                    }
+
+                    if(rand_on[0] == 'n' && cont_units[0] >= 1 && cont_ball[1] != 0){
+
+                        int check_range;
+
+                        check_range = cont_ball[1];
+
+                        if(rand_on[0] == 'n'){
+
+                            if(activePlay == 2 && check_range > 8){
+
+                                rand_on[0] = 'j';
+                                mode_player[0] = 'p';
+                                mode_player[1] = '9';
+
+                            }else if(activePlay == 2 && check_range < 8){
+                            
+                                rand_on[0] = 'j';
+                                mode_player[0] = 'p';
+                                mode_player[1] = '7';
+                            }
+                        
+                            if(activePlay == 1 && check_range > 8){
+                             
+                                rand_on[0] = 'm';
+                                mode_player[0] = 'p';
+                                mode_player[1] = '7';
+
+                            }else if(activePlay == 1 && check_range < 8){
+                            
+                                rand_on[0] = 'm';
+                                mode_player[0] = 'p';
+                                mode_player[1] = '9';
+                            }
+                        }   
+
+                        if(rand_on[0] == 'j' || rand_on[0] == 'm'){
+
+                            status_check[0] = 't';
+                        }
+
+                    }
+                    
+                }
+
+                if(status_check[0] != 'n'){
+              
+                    //show units
+                    count_units(renderer, vetX, vetY, cont_units, vetN, mode_player);                    
+                }
+
+                if(change_mode[0] == 'n'){
+
+                    int cont_pm = 1;
+
+                    for(int pm = 0; pm < 15; pm++){
+
+                        if(vetV[pm] == 0 && vetM[pm] > 3 && vetAX[pm] == 0 && vetAY[pm] == 0){
+                            cont_pm++;
+                        }
+                    }
+
+                    if(vetM[15] == 100 && vetV[15] == 0 && cont_pm > 15 && change_mode[0] == 'n'){
+                        
+                        //start check units
+                        int cp = 0, cm = 0, ca = 0;
+
+                        if(mode_player[1] == '7'){
+
+                            for(int p1 = 0; p1 < 15; p1++){
+            
+                                if(vetN[p1] < 8 && vetY[p1] != 0 && vetX[p1] != 0){
+                                    cp++;
+                                }
+
+                                if(vetN[p1] > 8 && vetY[p1] != 0 && vetX[p1] != 0){
+                                    cm++;
+                                }
+                            }
+
+                        }else if(mode_player[1] == '9'){
+
+                            for(int p2 = 0; p2 < 15; p2++){
+            
+                                if(vetN[p2] > 8 && vetY[p2] != 0 && vetX[p2] != 0){
+                                    cp++;
+                                }
+
+                                if(vetN[p2] < 8 && vetY[p2] != 0 && vetX[p2] != 0){
+                                    cm++;
+                                }
+                            }
+                        }
+
+                        for(int all = 0; all < 15; all++){
+
+                            if(vetX[all] == 0 && vetY[all] == 0){
+                                ca++;
+                            }
+                        }
+                        //end check units
+
+                        //start reset mode player x machine 
+                        if(activePlay == 1 && change_mode[0] == 'n'){
+
+                            if(mode_player[1] == '0' && ca == 0 || ca == 1){
+                                activePlay = 2;
+        
+                            }
+
+                            if(cm == cont_m[0] && mode_player[1] != '0'){
+                                activePlay = 2;
+                            }
+
+                            change_mode[0] = 't';
+
+                        }else if(activePlay == 2 && change_mode[0] == 'n'){
+
+                            if(mode_player[1] == '0' && ca == 0 || ca == 1){
+                                activePlay = 1;
+                            }
+                            
+                            if(cp == cont_p[0] && mode_player[1] != '0'){
+                                activePlay = 1; 
+                            }
+
+                            change_mode[0] = 't';
+                        }
+                        //end
+                    }
+                }
             }
         }
         //end reset mode off
 
         //start show in mode off
-        if(activePlay != 1 && vetM[15] > 3){
+        if(activePlay == 2 && vetM[15] > 3){
 
             rendFor(renderer, f);
 
@@ -380,11 +616,12 @@ int main(int argc, char *argv[]){
 
             rendEffects(renderer, e_x, e_y);
             rendTaco(renderer, vetX, vetY, deg, deg_mode, df_x, df_y, p_x, p_y);
+            count_units(renderer, vetX, vetY, cont_units, vetN, mode_player); 
         }
         //end show in mode off
 
         //Circle test
-        int circle_x;
+        /*int circle_x;
         int circle_y;
         int circle_radius;
 
@@ -410,7 +647,7 @@ int main(int argc, char *argv[]){
             }
 
             c++;
-        }
+        }*/
 
 		SDL_RenderPresent(renderer);
         SDL_Delay(6);
